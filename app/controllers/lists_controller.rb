92b5aa382092f2da4ea00645c163d7f15f4b84ca
yukari-ishii <115003222+yukari-ishii@users.noreply.github.com>
2022-10-10 14:05:25 +0000

    [Finish]3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,33 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+
+  def create
+    # 1.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
+
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
   end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+
 end
